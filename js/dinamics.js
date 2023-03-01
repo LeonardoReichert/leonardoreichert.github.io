278e7f5e5f10187db3b16fcf50c3c5a0baf04c0a
Leonardo Reichert <leoreichert5000@gmail.com>
2023-03-01 01:03:40 -0300

    añadido boton de copiar email, ticmass finalizado

--- js/dinamics.js
+++ js/dinamics.js
@@ -24,3 +24,28 @@ document.getElementById("btnShowPInfo").onclick = function(){
 
 
 
+
+
+// boton copiar !
+
+function copy(idElement, idBtnCopy){
+        
+        let ctrl = document.getElementById(idElement);
+
+        //ctrl.setSelectionRange(0, 99999); // For mobile devices
+
+        navigator.clipboard.writeText(ctrl.innerHTML);
+
+        // succes!
+
+        // cambiar el boton temporalmente a check_box y devolverlo a content_copy:
+        function changeStateButton(innerName){ //change innerHTML
+            document.getElementById(idBtnCopy).innerHTML = innerName;
+        }
+        changeStateButton("check_box");
+        
+        setTimeout(() => { changeStateButton("content_copy") }, 1000);
+}
+
+
+
